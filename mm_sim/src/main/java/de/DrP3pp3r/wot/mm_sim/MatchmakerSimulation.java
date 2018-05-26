e42053cc47e7d1aff7aebd2fefdda419bf7343c9
Wolfgang Petroschka <wp@seadex.de>
2018-05-26 20:22:19 +0200

    initial commit of mm_sim
    - setup Maven project
    - check build
    - run & check console output

---
+++ mm_sim/src/main/java/de/DrP3pp3r/wot/mm_sim/MatchmakerSimulation.java
@@ -0,0 +1,9 @@
+package de.DrP3pp3r.wot.mm_sim;
+
+public class MatchmakerSimulation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "MatchmakerSimulation was started!" );
+    }
+}
