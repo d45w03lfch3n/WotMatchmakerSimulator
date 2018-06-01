ddc3f31868beaff73f8d2b760dc4b739e0f2d776
Wolfgang Petroschka <wp@seadex.de>
2018-06-01 14:27:03 +0200

    MatchStore API

--- WotMatchmakerSimulator/src/main/java/de/DrP3pp3r/wot/WotMatchmakerSimulator/api/MatchStore.java
+++ WotMatchmakerSimulator/src/main/java/de/DrP3pp3r/wot/WotMatchmakerSimulator/api/MatchStore.java
@@ -1,5 +1,18 @@
 package de.DrP3pp3r.wot.WotMatchmakerSimulator.api;
 
+import de.DrP3pp3r.wot.WotMatchmakerSimulator.match.Match;
+import de.DrP3pp3r.wot.WotMatchmakerSimulator.tanks.TankType;
+
 public class MatchStore {
+
+public void storeMatch(Match match)
+{
+	
+}
+
+public void storeUnmatchedTank(TankType tankType)
+{
+	
+}
 
 }
